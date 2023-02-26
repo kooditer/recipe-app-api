cee8a0c1e143fbe15171bac6ae8e281b9162e345
kooditer <veiko.maesalu@gmail.com>
2023-02-26 10:37:53 +0100

    ei toimi3

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import SimpleTestCased
+from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
