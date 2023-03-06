26a3d336fe617be661ea286f68b804ffb5457f4f
kooditer <veiko.maesalu@gmail.com>
2023-03-06 13:05:16 +0100

    auth token error

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,8 +1,10 @@
 """
 serializers for the user api view
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (get_user_model,
+                                 authenticate)
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 class UserSerializer(serializers.ModelSerializer):
     """serializer for the user object"""
@@ -16,4 +18,24 @@ class UserSerializer(serializers.ModelSerializer):
         """create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
-
+class AuthTokenSerializer(serializers.Serializer):
+    """serializer for the user auth token"""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type':'password'},
+               trim_whitespace=False
+    )
+    def validate(self, attrs):
+        """validate and authenticate user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,17 @@
 """views for the user api
 """
 from rest_framework import generics
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.views import ObtainAuthToken
 
 class CreateUserView(generics.CreateAPIView):
     """create a new user in the system"""
     serializer_class = UserSerializer
 
+class CreateTokenView(ObtainAuthToken):
+    """create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
